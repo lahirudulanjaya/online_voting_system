d2f1d2bd2c7ea354e188188e11c2087e811ab81b
LahiruDulanjaya <laahirudulanjaya@gmail.com>
2018-09-11 15:36:16 +0530

    Revert "registration"
    This reverts commit 949932c7e3d3f8c573e67c39e2622a40d201f41a.

--- online_voting_system_registration/server/config/config.js
+++
@@ -1,4 +0,0 @@
-var env = process.env.NODE_ENV || 'development';
-var config =require('./config.json');
-var envConfig = config[env];
-Object.keys(envConfig).forEach(key => process.env[key]=envConfig[key]);
