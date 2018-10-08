2d0d9d63ccaf9230f8a77efe4c00787d570049c9
Sanduni Kariyawasam <sandunikariyawasam95@gmail.com>
2018-10-08 21:23:31 +0530

    quality control

--- Online_voting_system_registration/front/src/app/app.module.ts
+++ Online_voting_system_registration/front/src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule,HTTP_INTERCEPTORS} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { SignUpComponent } from './user/sign-up/sign-up.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
-import {UserService} from './shared/user.service';
+import { UserService } from './shared/user.service';
 import { SignInComponent } from './user/sign-in/sign-in.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
-import {ConfirmEqualValidatorDirective} from './shared/confirm-validate.directive';
+import { ConfirmEqualValidatorDirective } from './shared/confirm-validate.directive';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 import { MatGridListModule, MatFormFieldModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatDatepickerModule } from '@angular/material';
 import { LayoutModule } from '@angular/cdk/layout';
 import { AuthGuard } from './auth/auth.guard';
 import { AuthInterceptor } from './auth/auth.interceptor';
-import {SelectRequredValidatorDirective} from './shared/Select-validate.directive';
+import { SelectRequredValidatorDirective } from './shared/Select-validate.directive';
 import { ElectionComponent } from './admin/election/election.component';
 import { AdminComponent } from './admin/admin.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 
--- Online_voting_system_registration/front/src/app/shared/user.model.ts
+++ Online_voting_system_registration/front/src/app/shared/user.model.ts
@@ -5,5 +5,4 @@ export class User {
   phonenumber:string;
   password: string;
   cpassword:string;
-
 }
