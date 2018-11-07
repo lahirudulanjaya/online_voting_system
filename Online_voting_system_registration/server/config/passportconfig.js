804d74703d931f988f68abb9b895e3d2ef41d40c
root <laahirudulanjaya@gmail.com>
2018-11-08 02:59:00 +0530

    AT commands

--- Online_voting_system_registration/server/config/passportconfig.js
+++ Online_voting_system_registration/server/config/passportconfig.js
@@ -3,6 +3,14 @@ const localStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 
 var User = mongoose.model('user');
+var SerialPort = require('serialport');
+var otpGenerator = require('otp-generator')
+var otp =otpGenerator.generate(6, { upperCase: false, specialChars: false });
+var port = new SerialPort("/dev/ttyUSB0", {
+  baudRate: 9600,
+  dataBits: 8,
+  parity: 'none'
+});
 
 passport.use(
     new localStrategy({ usernameField: 'userName' },
@@ -21,8 +29,32 @@ passport.use(
                     else if(!user.verifyEmail())
                         return done(null,false, {message : 'please verify email address'});
                     // authentication succeeded
-                    else
+                    else{
+                        
+
+                            console.log('Serial communication open');
+                            port.write("AT");
+                            port.write('\r');
+                            port.write("AT+CMGF=1");
+                            port.write('\r');
+                            port.write("AT+CMGS=\"" + "0758034032" + "\"");
+                            port.write('\r');
+                            port.write(otp); 
+                            port.write(Buffer([0x1A]));
+                            port.write('^z');
+                        console.log(otp);
                         return done(null, user);
+
+                    }
                 });
         })
 );
+function gsm_message_sending(serial, message ,phone_no) {
+    serial.write("AT+CMGF=1");
+     serial.write('\r');
+     serial.write("AT+CMGS=\"" + phone_no + "\"");
+     serial.write('\r');
+     serial.write(message); 
+     serial.write(Buffer([0x1A]));
+     serial.write('^z');
+ }
