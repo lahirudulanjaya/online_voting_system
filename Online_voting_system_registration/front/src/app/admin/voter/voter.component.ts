a7a7ee0c5be5711b52d4210603d49d2b8b6a4251
Sanduni Kariyawasam <sandunikariyawasam95@gmail.com>
2019-01-06 20:53:21 +0530

    change to cursor on hover

--- Online_voting_system_registration/front/src/app/admin/voter/voter.component.ts
+++ Online_voting_system_registration/front/src/app/admin/voter/voter.component.ts
@@ -11,33 +11,31 @@ import { MatDialog } from '@angular/material';
   templateUrl: './voter.component.html',
   styleUrls: ['./voter.component.css']
 })
+
 export class VoterComponent implements OnInit {
   serverErrorMessages: string;
   showSucessMessage: boolean;
   users: User[];
 
-  constructor(private userService: UserService, private router: Router, public dialog: MatDialog) { }
-  displayedColumns: string[] = ['userName', 'registrationnumber', 'phonenumber','email'];
+  constructor(private userService: UserService, private router: Router, public dialog: MatDialog) {  }
+  displayedColumns: string[] = ['userName', 'registrationnumber', 'phonenumber', 'email'];
   dataSource = this.users;
-
-  
-
-  
 
   ngOnInit() {
     this.userService.getUserProfiles().subscribe(
-      users =>{
-          this.users=users as User[];
+      users => {
+        this.users = users as User[];
       })
-      }
-      Onedit(member: User) {
-        this.userService.selectedUser = member;
-      }
-      Onsubmit(form: NgForm) {
-        this.userService.putuserprofile(form.value);
-      }
-      Ondelete(id: string) {
-        this.userService.deleteuserprofile(id);
-      }
   }
+
+  Onedit(member: User) {
+    this.userService.selectedUser = member;
+  }
+  Onsubmit(form: NgForm) {
+    this.userService.putuserprofile(form.value);
+  }
+  Ondelete(id: string) {
+    this.userService.deleteuserprofile(id);
+  }
+}
 
